ad50880b73142758b38e6035413106958709fab4
vlad.smelyanets <vlad.smelyanets@gmail.com>
2021-02-10 16:57:57 +0300

    feat: add Publisher - books relation

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,27 +25,34 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Publisher lenizdat = new Publisher("Lenizdat", "Fontanka 59", "Leningrad", "Leningradskaya", "300300");
+        publisherRepository.save(lenizdat);
+
         Author leoTolstoy = new Author("Leo", "Tolstoy");
         Book warAndPeace = new Book("War and Peace", "123123");
         leoTolstoy.getBooks().add(warAndPeace);
         warAndPeace.getAuthors().add(leoTolstoy);
+        warAndPeace.setPublisher(lenizdat);
+        lenizdat.getBooks().add(warAndPeace);
 
         authorRepository.save(leoTolstoy);
         bookRepository.save(warAndPeace);
+        publisherRepository.save(lenizdat);
 
         Author fedorDostoyevski = new Author("Fedor", "Dostoyevski");
         Book idiot = new Book("Idiot", "679034");
         fedorDostoyevski.getBooks().add(idiot);
         idiot.getAuthors().add(fedorDostoyevski);
+        idiot.setPublisher(lenizdat);
+        lenizdat.getBooks().add(idiot);
 
         authorRepository.save(fedorDostoyevski);
         bookRepository.save(idiot);
-
-        Publisher lenizdat = new Publisher("Lenizdat", "Fontanka 59", "Leningrad", "Leningradskaya", "300300");
         publisherRepository.save(lenizdat);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books in DB: " + bookRepository.count());
         System.out.println("Number of Publishers in DB: " + publisherRepository.count());
+        System.out.println(lenizdat.getName() + " number of Books :" + lenizdat.getBooks().size());
     }
 }
